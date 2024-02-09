53c3bb3ae75dfd01eb44777ae601451967870ab5
kinauanne <159478938+kinauanne@users.noreply.github.com>
2024-02-09 17:03:57 +0000

    insert tutores kinauanne

---
+++ code/sql/population/insert_tutores_kinauanne.sql
@@ -0,0 +1,12 @@
+INSERT INTO tutores ("id_tutor","cpf_tutor", "nome_tutor", "email_tutor", "telefone_tutor", "cargo", "formacao")
+VALUES 
+	('01', '47101788017', 'Peter Parker', 'peter@gmail.com', '62954487512', 'Coordenado','Psicologia'),
+	('02', '25110458006', 'Draco Malfoy', 'draco@gmail.com', '62914587925', 'Estagiário','Pedagogia'),
+	('03', '05037893054', 'Tony Stark', 'tony@gmail.com', '62958741598', 'Supervisor','Gestão Escolar'),
+	('04', '32871266018', 'Bruce Wayne', 'bruce@gmail.com', '62985741369', 'Supervisor','Psicologia'),
+	('05', '73086269030', 'Harry Potter', 'harry@gmail.com', '62958795869', 'Estagiário','Gestão Escolar'),
+	('06', '08606643092', 'Mary Jane', 'mary@gmail.com', '62968693528', 'Coordenado','Psicologia'),
+	('07', '21502645017', 'Severus Snape', 'severus@gmail.com', '62936495174', 'Supervisor','Pedagogia'),
+	('08', '45334848088', 'Hermione Granger', 'hermione@gmail.com', '62964852136', 'Coordenado','Pedagogia'),
+	('09', '83276472023', 'Natasha Romanova', 'natasha@gmail.com', '62998456132', 'Estagiário','Psicologia'),
+	('10', '89926533019', 'Loki Laufeyson', 'loki@gmail.com', '62995476158', 'Coordenado','Gestão Escolar');
