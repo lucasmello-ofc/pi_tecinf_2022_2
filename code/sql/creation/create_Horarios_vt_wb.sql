fa4731b6fb6bb1dc32e5c41de32fe6662352f108
Prof. Danielle Almeida <159446836+profdaniellegms@users.noreply.github.com>
2024-02-14 14:09:15 -0300

    correção prof. create_Horarios_vt_wb.sql

--- code/sql/creation/create_Horarios_vt_wb.sql
+++ code/sql/creation/create_Horarios_vt_wb.sql
@@ -1,14 +1,9 @@
 create table horarios(
 	id_horario varchar(11) primary key,
-<<<<<<< HEAD
 	hora_inicio time NOT null,
 	hora_fim time NOT NULL,
-	dia_semana varchar(20), 
-	check ( dia_semana in ('Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sabado'))
-)
-=======
-	hora_inicio time NOT NULL,
-	hora_fim time NOT NULL,
-	dia_semana varchar(20)
-)
->>>>>>> 92ccba266d4afe4babe1c317e29d91a3a3b0c64c
+	dia_semana varchar(20), check ( 
+		dia_semana in ('Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 
+		'Quinta-Feira', 'Sexta-Feira', 'Sabado')
+	)
+);
