6a89a81b87daa35fd72b74e5882ae97b121d1786
MarcusVinniciussk <159478906+MarcusVinniciussk@users.noreply.github.com>
2024-02-20 19:08:20 +0000

    create_views_responsaveis.sql

--- code/sql/exercices/views/create_views_responsaveis.sql
+++ code/sql/exercices/views/create_views_responsaveis.sql
@@ -1,3 +1,6 @@
+--No Result Set o nome do responsável e a quantidade de alunos,
+--Do maior para o menor e em ordem alfabética pelo nome do responsável.
+
 CREATE VIEW contar_responsaveis AS
 SELECT nome_responsavel, COUNT(*) AS contar
 FROM alunos
@@ -5,6 +8,7 @@ LEFT JOIN responsaveis ON alunos.fk_responsavel = responsaveis.id_responsavel
 group by nome_responsavel
 order by nome_responsavel asc, contar desc;
 
+--O Result Set deverá conter as seguintes colunas: ‘nome do responsável’ e ‘nome do aluno’.
 
 CREATE VIEW listar_responsaveis AS
 SELECT nome_responsavel, nome_aluno
@@ -12,6 +16,7 @@ FROM alunos
 LEFT JOIN responsaveis ON alunos.fk_responsavel = responsaveis.id_responsavel
 order by nome_responsavel;
 
+--O Result Set deverá conter o nome do responsável e a quantidade de alunos.
 
 CREATE VIEW qtde_media AS
 SELECT contar_responsaveis.nome_responsavel, contar
