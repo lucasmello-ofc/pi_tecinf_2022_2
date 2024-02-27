9708ead93c9c66a3264c5bafc425ec8f8cfa3a08
lucasmello-ofc <lucasdusis122@gmail.com>
2024-02-27 16:55:33 -0300

    update

--- code/backend/src/models/tutores_horarios.ts
+++ code/backend/src/models/tutores_horarios.ts
@@ -1,28 +1,22 @@
-import { Entity, PrimaryColumn, ManyToOne, JoinColumn, Column } from "typeorm"
+import { Entity, Column, PrimaryColumn, } from "typeorm"
 import { Tutor } from "./tutor"
 import { Horario } from "./horario"
 
 @Entity("tutores_horarios")
-export class TutoreHorario { // Corrigido o nome da classe para TutoreHorario
+export class TutoreHorario { 
 
-    @Column({
+    @PrimaryColumn({
         type: "varchar",
         nullable: false
     })
     fk_tutore: string
 
-    @Column({
+    @PrimaryColumn({
         type: "varchar",
         nullable: false
     })
     fk_horario: string
 
-    @ManyToOne(() => Tutor, tutor => tutor)
-    @JoinColumn({ name: "fk_tutore" }) 
-    tutor: Tutor
-
-    @ManyToOne(() => Horario, horario => horario)
-    @JoinColumn({ name: "fk_horario" })
-    horario: Horario
+    
 }
 
