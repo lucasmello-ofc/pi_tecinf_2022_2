e0e29c53e83e0ef46357ab33fac1054cb9ab9636
Bruno <alvesbrunodesouza@gmail.com>
2024-02-27 16:09:22 -0300

    update

--- code/backend/src/connections/db_dev.ts
+++ code/backend/src/connections/db_dev.ts
@@ -1,12 +1,13 @@
 import { DataSource } from "typeorm"
 import { Necessidade } from "../models/necessidade"
+import { AlunoNecessidade } from "../models/aluno_necessidade"
 
 const DevDataSource = new DataSource({
     type: "postgres",
     host: "localhost",
-    port: 5432,
+    port: 5433,
     username: "postgres",
     password: "postgres",
     database: "inclusimap",
-    entities: [Necessidade]
+    entities: [Necessidade, AlunoNecessidade]
 })
