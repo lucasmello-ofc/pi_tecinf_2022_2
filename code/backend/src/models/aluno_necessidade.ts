dd6ec260d4028c0dc3655e828feed76ef7780418
luceliaandrade <luceliara@hotmail.com>
2024-02-28 17:08:56 -0300

    typeorm lucelia

--- code/backend/src/models/aluno_necessidade.ts
+++ code/backend/src/models/aluno_necessidade.ts
@@ -1,11 +1,15 @@
-import { Entity,PrimaryColumn, Column } from "typeorm"
+import { Entity,PrimaryColumn, Column, ManyToMany, ManyToOne } from "typeorm"
+import { Necessidade } from "./necessidade"
+import { Aluno } from "./aluno"
 
 @Entity("alunos_necessidades")
 export class AlunoNecessidade {
 
+    @ManyToOne(() => Aluno, aluno => aluno.id_aluno)
     @PrimaryColumn({ type: "varchar" })
     fk_aluno: string
 
-    @PrimaryColumn({ type: "varchar })
+    @ManyToOne(() => Necessidade, necessidade => necessidade.id_necessidade)
+    @PrimaryColumn({ type: "varchar" })
     fk_necessidade: string
 }
