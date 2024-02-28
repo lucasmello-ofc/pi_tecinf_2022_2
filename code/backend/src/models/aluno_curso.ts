bde188f128d2e8a9a831aaa582fa31df748b46de
João Paulo Ferreira da Silva <jpferreira293@gmail.com>
2024-02-28 17:08:59 -0300

    typeorm joaop

--- code/backend/src/models/aluno_curso.ts
+++ code/backend/src/models/aluno_curso.ts
@@ -1,6 +1,11 @@
-import { Entity, PrimaryColumn, Column } from "typeorm"
+import { Entity, PrimaryColumn, Column, ManyToOne } from "typeorm"
+import {v4 as uuid } from "uuid"
+import { Check } from "typeorm"
+import { Aluno } from "./aluno"
+import { Curso } from "./curso"
 
 @Entity("alunos_cursos")
+@Check("situacao IN ('Matriculado','Concluído','Evadido', 'Trancado')")
 export class AlunoCurso {
 
     @PrimaryColumn({ type: "varchar" })
@@ -9,9 +14,29 @@ export class AlunoCurso {
     @Column({ type: "varchar", nullable: false })
     situacao: string
 
+    @ManyToOne(()=> Aluno, alunos => alunos.id_aluno)
     @Column({ type: "varchar", nullable: false })
     fk_aluno: string
-    
+
+    @ManyToOne(()=> Curso, cursos => cursos.id_curso)
     @Column({ type: "varchar", nullable: false })
     fk_curso: string
+
+    @Column({type:"timestamptz", nullable: false })
+    dataCriacao: Date
+
+    @Column({type:"timestamptz", nullable: false})
+    dataUltimaAlteracao: Date
+
+    @Column({type:"timestamptz", nullable: false})
+    dataExclusao: Date
+
+    constructor (){
+        this.matricula = uuid()
+        this.dataCriacao = new Date()
+        this.dataUltimaAlteracao
+        this.dataExclusao
+    }
 }
+
+
