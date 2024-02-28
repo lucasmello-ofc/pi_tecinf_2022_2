8b1dbb8c8b430fb3c1c62962fa6eeffaae9249d9
rafaelfariasoliver <rafaelfariasdeoliveira310@gmail.com>
2024-02-28 17:13:45 -0300

    typeormrafael

--- code/backend/src/models/curso.ts
+++ code/backend/src/models/curso.ts
@@ -1,12 +1,16 @@
-import { Entity, PrimaryColumn, Column } from "typeorm"
-
-@Entity("cursos")
+import { Entity, PrimaryColumn, Column, Check } from "typeorm"
+"typeorm"
+import { v4 as uuid } from "uuid"
+@Check("turno IN ('Matutino', 'Vespertino', 'Noturno')")
+@Check("modalidade IN ('Iniciação', 'Capacitação', 'Qualificação','Aperfeiçoamento', 'Técnico', 'Superior - Licenciatura','Superior - Bacharel', 'Superior - Tecnologia','Pós Graduação Lato Sensu - Especialização','Pós-Graduação Stricto Sensu - Mestrado','Pós-Graduação Stricto Sensu - Doutorado')"
+)
 export class Curso {
     @PrimaryColumn({ type: "varchar" })
     id_curso :string
     @Column({ type: "varchar", length: 255, nullable: false })
     descricao_curso: string
     @Column({ type: "varchar", length: 15, nullable: false })
+    
     turno: string
     @Column({ type: "varchar", length: 30, nullable: false })
     modalidade: string
@@ -14,8 +18,23 @@ export class Curso {
     eixo_dpto:string
     @Column({ type: "varchar", length: 50, nullable: false })
     unidade_campus: string
-    @Column({ type: "bool" })
+    @Column({ type: "date" })
     semestral: boolean
-    @Column({ type: "bool" })
+    @Column({ type: "date" })
     presencial: boolean
+    @Column({ type: "timestamptz"  })
+    dataCriacao: Date
+    @Column({ type: "timestamptz" })
+    dataUltimaAlteracao: Date
+    @Column ({ type: "timestamptz" })
+    dataExclusao: Date
+
+    constructor(){
+        this.id_curso = uuid()
+        this.dataCriacao = new Date()
+        this.dataUltimaAlteracao
+        this.dataExclusao
+    }
 }
+
+
