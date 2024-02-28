b570bcd973cb329c91b16ad94d88ce0a4740f317
verton11 <verton245@gmail.com>
2024-02-28 17:10:37 -0300

    typeorm weverton

--- code/backend/src/models/horario.ts
+++ code/backend/src/models/horario.ts
@@ -1,16 +1,31 @@
-import { Entity, PrimaryColumn, Column } from "typeorm"
+import { Entity, PrimaryColumn, Column, Check} from "typeorm"
+import { v4 as uuid } from "uuid"
 
 @Entity("horarios")
+@Check("dia_seman  in ('Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sabado')")
 export class Horario {
     @PrimaryColumn({ type: "varchar" })
     id_horario : string
 
-    @Column({ type: "time", nullable: true })
+    @Column({ type: "time", nullable: false })
     hora_incio: Date
 
-    @Column({ type: "time", nullable: true })
+    @Column({ type: "time", nullable: false })
     hora_fim : Date
-
+    
     @Column({ type: "varchar", length: 20 })
     dia_semana:string
+
+    @Column({ type: "timestamptz", nullable: false })
+    dataCriacao: Date
+
+    @Column({ type: "timestamptz", nullable: false })
+    dataUltimaAltercao: Date
+
+    @Column({ type: "timestamptz", nullable: false })
+    dataExclusao: Date
+
+    constructor(){
+        this.id_horario = uuid()
+    }
 }
